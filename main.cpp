53a6e4e58ccb5d8ee44e2d618eec9c7a5e7b14bd
Ryan Kim <mwkjello@gmail.com>
2026-01-21 12:29:41 -0500

    updated main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <vector>
 #include<cstdlib>
 #include <ctime>
+#include<iostream>
 using namespace std;
 
 
 class code {
 
     private: 
-        std::vector<int> content;
+        vector<int> content;
         int n;
         int m;
     public:
@@ -22,4 +23,29 @@ class code {
                 content[i] = rand() % m;
             }
         }
+
+        void print() const {
+            for (int i = 0; i < n; i++) {
+                cout << content[i] << (i < n - 1 ? ", " : "");
+            }
+            cout << endl;
+        }
 };
+
+int main() {
+    srand(time(0));
+    int n, m;
+
+    cout << "How many digits: ";
+    cin >> n;
+    cout << endl;
+    cout << "What's the range?: ";
+    cin >> m;
+    cout << endl;
+
+    code secretCode(n, m);
+    secretCode.initializeRandom();
+    secretCode.print();
+    return 0;
+
+}
